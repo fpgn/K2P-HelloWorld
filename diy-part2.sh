6d7bfa80722aa633e5a25df57fa5a4921532bc3a
i5g5vip <58246606+i5g5vip@users.noreply.github.com>
2020-07-25 17:55:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 去除默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/+luci-theme-bootstrap/ /g' feeds/luci/collections/luci/Makefile
